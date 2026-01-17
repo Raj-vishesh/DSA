be8803e11eed0c159ac68f083fdb3b9965ef5cd4
Raj-vishesh <visheshtopper99@gmail.com>
2026-01-17 23:35:48 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (38.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,22 +1,68 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        string cleaned = "";
 
-        for(char c : s){
-            if(isalnum(c)){     //     isalnum()  ->   is used to check for function , punctuation and spaces and numbers
-                cleaned += tolower(c);              // tolower(c) ->    is used to conver upperCase to lowercase
+        // string cleaned = "";
+
+        // for (char c : s) {
+        //     if (isalnum(
+        //             c)) { //     isalnum()  ->   is used to check for function ,
+        //                   //     punctuation and spaces and numbers
+        //         cleaned += tolower(c); // tolower(c) ->    is used to conver
+        //                                // upperCase to lowercase
+        //     }
+        // }
+
+       
+
+        // string p = cleaned;
+
+        // reverse(cleaned.begin(), cleaned.end());
+
+        // if (cleaned == p)
+        //     return true;
+        // else
+        //     return false;
+
+
+
+
+        int i = 0; 
+        int j = s.length() - 1 ;
+
+        while(i <= j){
+            
+            char left = s.at(i);
+
+            char right = s.at(j);
+
+
+            if(!isalnum(left)){
+                i++;
+                continue;
             }
+
+            if(!isalnum(right)){
+                j--;
+                continue;
+            }
+
+            if(tolower(left)  != tolower(right)){
+                return false;
+                
+            }
+
+            i++;
+
+            j--;
+
         }
 
-        string p = cleaned;
 
-        
 
-        reverse(cleaned.begin() ,cleaned.end());
+        return true;
 
-        if( cleaned== p) return true ;
-        else return false;
+
+
     }
 };
