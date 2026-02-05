d427f41fc3a5bd15afc05b3876328eabce4566ba
Raj-vishesh <visheshtopper99@gmail.com>
2026-02-06 00:09:38 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (80.55%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,53 @@
+class Solution {
+
+    void dfs(int r , int c , vector<vector<char>> &board){
+
+        int n = board.size();
+        int m = board[0].size();
+
+        if(r < 0 || c < 0 || r >= n || c >= m || board[r][c] != 'O' ){
+            return;
+        }
+
+        board[r][c] = '#';
+
+        dfs(r-1 , c, board);
+        dfs(r, c+1 , board);
+        dfs(r+1 , c , board);
+        dfs(r, c-1 , board);
+
+    }
+
+
+public:
+
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        vector<vector<char>> ans;
+
+
+        for(int i = 0 ; i < n ; i++){
+            dfs(i , 0 , board);
+            dfs(i , m-1 , board);
+        }
+
+        for(int j = 0 ; j < m ; j++){
+            dfs(0 , j , board);
+            dfs(n-1 , j , board);
+        }
+
+        for(int i = 0  ; i< n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+
+                if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+            }
+        } 
+    }
+};
