12e5e0bc4e36595d561fe65fc577f759ab9c0127
Raj-vishesh <visheshtopper99@gmail.com>
2026-09-14 21:57:42 +0530

    Time: 925 ms (17.17%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,7 +1,7 @@
-SELECT product_id, year as first_year, quantity, price 
-FROM Sales 
-WHERE (product_id, year) in (
-    SELECT product_id, MIN(year) 
-    FROM Sales 
-    GROUP BY product_id
-);
+select product_id , year as first_year , quantity, price 
+from Sales 
+where (product_id , year) in (
+    select product_id , min(year) as f_year
+    from Sales
+    group by product_id
+)
